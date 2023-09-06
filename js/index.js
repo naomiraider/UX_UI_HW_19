768dd4e4325cc06493c28e5fe81b96348d0849fe
naomiraider <97920424+naomiraider@users.noreply.github.com>
2023-09-06 13:04:19 -0700

    resume

--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
-console.log("your index.js file is loaded correctly")
+console.log("your index.js file is loaded correctly");
 
+$("#resume").click(function () {
+  window.open(
+    "https://acrobat.adobe.com/link/review?uri=urn:aaid:scds:US:bcf94fbc-748e-3738-88bc-cdf2c6b842ca",
+    "_blank"
+
+  );
+});
